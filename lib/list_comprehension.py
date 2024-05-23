078dcbc8668853d62c990d6cb1c50859c2ca2adc
+ <+>
2024-05-23 18:46:39 +0300

    changes

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,9 +2,12 @@
 
 def return_evens(num_list):
      return [n for n in num_list if n%2 == 0]
-   
+
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))   
     
 
 
 def make_exclamation(sentence_list):
  return [sentence + "!" for sentence in sentence_list]
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
